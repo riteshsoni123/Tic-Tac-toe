d1f269879190df52288608eff80a30b3c856f4f0
riteshsoni123 <comedybuddy1@gmail.com>
2021-10-21 15:32:13 +0530

    made some changes in the click event

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
 
-var card=new Array(3);
-for(var i=0;i<3;i++){
-    card[i]=new Array(3);
-}
-
-card[0][0]=document.querySelector('#card00');
-card[0][1]=document.querySelector('#card01');
-card[0][2]=document.querySelector('#card02');
-card[1][0]=document.querySelector('#card10');
-card[1][1]=document.querySelector('#card11');
-card[1][2]=document.querySelector('#card12');
-card[2][0]=document.querySelector('#card20');
-card[2][1]=document.querySelector('#card21');
-card[2][2]=document.querySelector('#card22');
 var reset=document.querySelector('.reset');
 var result1=document.querySelector('#result1');
 var result2=document.querySelector('#result2');
 var count=0;
-
+var card=document.getElementsByClassName('card');
 const check_status=()=>{
-    var card1=document.getElementsByClassName('card');
     var win=[
         [0,1,2,16,1,0,1],
         [3,4,5,48,1,0,1],
@@ -31,9 +16,7 @@ const check_status=()=>{
         [2,4,6,48,0,-45,1]
     ];
     win.forEach(e=>{
-        // console.log(card1[e[0]] );
-        if((card1[e[0]].innerHTML==card1[e[1]].innerHTML&&card1[e[1]].innerHTML==card1[e[2]].innerHTML)&&(card1[e[0]].innerHTML!="")){
-            // console.log('bazinga');
+        if((card[e[0]].innerHTML==card[e[1]].innerHTML&&card[e[1]].innerHTML==card[e[2]].innerHTML)&&(card[e[0]].innerHTML!="")){
             if(e[6]==1){
                 result1.style.top=`${e[3]}%`;
                 result1.style.left=`${e[4]}%`;
@@ -50,66 +33,25 @@ const check_status=()=>{
     })
 }
 
-function change(i,j){
-    if(card[i][j].innerHTML==''){
-        if(count%2==0){
-            card[i][j].innerHTML='X';
+Array.from(card).forEach(element=>{
+    element.addEventListener('click',()=>{
+        if(element.innerHTML==''){
+            if(count%2==0){
+                element.innerHTML='X';
+            }
+            else{
+                element.innerHTML='O';
+            }
+            check_status();
+            count++;
         }
-        else{
-            card[i][j].innerHTML='O';
-        }
-        count++;
-    }
-}
-
-card[0][0].addEventListener('click', function (event) {
-    change(0,0);
-    check_status();
+    })
 });
-card[0][1].addEventListener('click', function (event) {
-    change(0,1);
-    check_status();
-});
-card[0][2].addEventListener('click', function (event) {
-    change(0,2);
-    check_status();
-});
-card[1][0].addEventListener('click', function (event) {
-    change(1,0);
-    check_status();
-});
-card[1][1].addEventListener('click', function (event) {
-    change(1,1);
-    check_status();
-});
-card[1][2].addEventListener('click', function (event) {
-    change(1,2);
-    check_status();
-});
-card[2][0].addEventListener('click', function (event) {
-    change(2,0);
-    check_status();
-});
-card[2][1].addEventListener('click', function (event) {
-    change(2,1);
-    check_status();
-});
-card[2][2].addEventListener('click', function (event) {
-    change(2,2);
-    check_status();
-});
-
 
 reset.addEventListener('click',function(event){
-    card[0][0].innerHTML="";
-    card[0][1].innerHTML="";
-    card[0][2].innerHTML="";
-    card[1][0].innerHTML="";
-    card[1][1].innerHTML="";
-    card[1][2].innerHTML="";
-    card[2][0].innerHTML="";
-    card[2][1].innerHTML="";
-    card[2][2].innerHTML="";
+    Array.from(card).forEach(element=>{
+        element.innerHTML='';
+    });
     result1.style.display='none';
     result2.style.display='none';
     result1.style.transform='rotate(0)'
